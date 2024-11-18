40a4f8600a80dbec4828f72426475ea072a45571
awaisirshad1 <awaisirshad1@icloud.com>
2024-11-18 14:32:47 -0500

    cleaned up home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
-// import { supabase } from "@/lib/supabase";
 import Library from "./library/page";
 import { cookies } from "next/headers";
 export default async function Home() {
-  
-  // const testSupabase = async () => {
-  //   const {data, error} = await supabase
-  //     .from("09_events")
-  //     .select()
 
-  //   if(data) console.log(data)
-  // }
-  
-  // testSupabase()
   const cookieStore = await cookies()
   return (
     <Library/>
