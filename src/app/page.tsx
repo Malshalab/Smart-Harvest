ffb8e9efd1481274ebb34f5eb5313cbfd19ef706
awaisirshad1 <awaisirshad1@icloud.com>
2024-11-18 14:29:46 -0500

    cleaned up some directories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
-import Library from "./resources/library";
+// import { supabase } from "@/lib/supabase";
+import Library from "./library/page";
+import { cookies } from "next/headers";
+export default async function Home() {
+  
+  // const testSupabase = async () => {
+  //   const {data, error} = await supabase
+  //     .from("09_events")
+  //     .select()
 
-export default function Home() {
+  //   if(data) console.log(data)
+  // }
+  
+  // testSupabase()
+  const cookieStore = await cookies()
   return (
     <Library/>
   );
