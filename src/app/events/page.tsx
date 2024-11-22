465147c4e3ccd6dec247e0d9949e26af6cd85f4f
awaisirshad1 <awaisirshad1@icloud.com>
2024-11-22 11:38:23 -0500

    cleaned up events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -2,17 +2,5 @@ import { sh_eventUpdated } from '@/data/event';
 import { getEventById, getEvents, createEvent, deleteEvent, updateEvent } from '../../pages/api/events/actions'
 
 export default async function Page() {
-    // createEvent({
-    //     eventName: "dummyEvent1",
-    //     eventDescription: 'dummy event 1 to test api',
-    //     eventDate: new Date(),
-    //     creationTimeStamp: new Date()
-    // })
-
-    const eventUpdated:sh_eventUpdated = {
-        eventId:3,
-        eventName:'updated event name',
-        
-    }
-    updateEvent(eventUpdated);
+    
 } 
